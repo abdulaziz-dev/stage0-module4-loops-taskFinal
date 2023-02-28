06b9fb7a0797fe6a3bf4c75683bf451e3797bbb4
abdulaziz-dev <axmedovabdulaziz2001@gmail.com>
2023-02-28 16:17:17 +0500

    loops finale

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,17 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        int z = 1;
+        for(int i=1; i<=cathetusLength; i++){
+            for(int j=cathetusLength-1; j>=i; j--){
+                System.out.print(" ");
+            }
+            for (int k = z; k>=1; k--){
+                System.out.print(Math.abs(k-i)+1);
+            }
+            z += 2;
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
